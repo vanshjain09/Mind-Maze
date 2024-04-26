df11c7128d707f4027ac436e16616ee9ee0cd84a
vanshjain09 <vanshjain499@gmail.com>
2024-04-26 13:17:21 +0530

    start adding frontend

--- quizapp/src/main/java/com/vansh/quizapp/controller/IndexController.java
+++ quizapp/src/main/java/com/vansh/quizapp/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.vansh.quizapp.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@ComponentScan(basePackages = {"/quizapp/src/main/java/com/vansh/quizapp/controller/IndexController.java"})
 public class IndexController {
+
     @GetMapping("/")
-    public String welcome() {
-        return "welcome"; // Assuming "welcome.html" is your template name
+    public String index() {
+        return "index"; // Assuming "index.html" is your template name
     }
-    
+    @PostMapping("/role")
+    public String role(@RequestParam("role") String role) {
+        System.out.println(role); // Changed to println for better visibility
+        return role;
+    }
 }
